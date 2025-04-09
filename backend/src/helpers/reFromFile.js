53ec4aacbbf60e9492120dde411da0ea44da7605
kenanmusali <149934213+kenanmusali@users.noreply.github.com>
2025-04-09 14:48:21 +0400

    Delete backend directory

--- backend/src/helpers/reFromFile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-function readFromFile(dbName) {
-    const data = fs.readFileSync("./src/db/index.json");
-    const jsonData = JSON.parse(data);
-    return jsonData[dbName];
-}
-
-module.exports = readFromFile;
